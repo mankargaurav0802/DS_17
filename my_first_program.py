3d12c79ec899c433a505fe41c65371455408386d
Rahul Thakur <rahulthakur02k@gmail.com>
2023-01-09 19:38:19 +0530

    adding notebooks

---
+++ my_first_program.py
@@ -0,0 +1,14 @@
+# below code is used for printing
+# this is my second comment
+# this is my third comment
+
+""" below code is used for printing
+this is my second comment
+this is my third comment """
+
+print("Hello this will get executed")
+
+
+
+
+
