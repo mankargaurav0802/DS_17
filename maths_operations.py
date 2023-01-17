d5653fc150b2ad5f8b3b9dbd8116785a74499064
Rahul Thakur <rahulthakur02k@gmail.com>
2023-01-17 21:10:12 +0530

    adding dictionary practice problems and modules

---
+++ maths_operations.py
@@ -0,0 +1,13 @@
+
+
+def summation(num1,num2):
+    return num1+ num2
+
+def subtract(num1,num2):
+    return num1-num2
+
+def multiply(num1,num2):
+    return num1*num2
+
+def divide(num1,num2):
+    return num1/num2
