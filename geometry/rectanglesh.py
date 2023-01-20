756b6f8a2e60b6206be43cfdd806966c43fecd74
shubhamwakade <122179741+shubhamwakade@users.noreply.github.com>
2023-01-20 18:57:06 +0530

    Create rectanglesh.py
    shubham change

---
+++ geometry/rectanglesh.py
@@ -0,0 +1,13 @@
+def Width(x, width):
+        x.width = width
+
+def Length(x, length):
+        x.length = length
+
+def Perimeter(x):
+        return (2 * x.length) + (2 * x.width)
+
+def Area(x):
+       return x.length * x.width
+
+ 
