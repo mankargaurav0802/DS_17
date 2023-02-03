0109cacf342faee58dbb269943517158d9a89d39
mankargaurav0802 <122684761+mankargaurav0802@users.noreply.github.com>
2023-02-03 18:42:57 +0530

    opps
    object oriented programs

---
+++ geometry/importguru.py
@@ -0,0 +1,2 @@
+from squarr import area_of_square
+area_of_square(8)
---
+++ geometry/squarr.py
@@ -0,0 +1,7 @@
+def area_of_square(side):
+    Area = side * side
+    print ("if side of square {} then area of square is {}".format(side,Area))
+def perimeter_of_square(side):
+    Perimeter = 4 * side
+    print "if side of square {} then perimeter of square {}".format
+perimeter_of_square(8)
